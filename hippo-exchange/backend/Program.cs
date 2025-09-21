a81f6ed249648b0f572b912e07a1f82114e8fe4a
michaelserdar <122762214+michaelserdar@users.noreply.github.com>
2025-09-21 13:07:47 -0500

    updated apsettings.json with firestore DatabaseID for connection

--- hippo-exchange/backend/Program.cs
+++ hippo-exchange/backend/Program.cs
@@ -22,7 +22,7 @@ namespace HippoExchange
             var databaseId =
                 Environment.GetEnvironmentVariable("FIRESTORE_DATABASE_ID")
                 ?? builder.Configuration["GoogleCloud:DatabaseId"]
-                ?? "(default)";
+                ?? "group13capstone";
 
 
             // Services
